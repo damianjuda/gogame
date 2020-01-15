f1610725a5358d8b9dd83ffc34b5e791be839eaa
Damian Juda <damianjuda@gmail.com>
2020-01-15 14:40:07 +0100

    add comments

--- game/board.go
+++ game/board.go
@@ -3,14 +3,14 @@ package game
 type dir int
 
 const (
-	UD dir = 0
-	LR dir = 1
+	UD dir = 0 // pionowe domino
+	LR dir = 1 // poziome domino
 )
 
 type domino struct {
-	index int
-	dir   dir
-	board *Board
+	index int // indeks na 1 wymiarowej tablicy (reprezentującej 2 wymiarową) prawy górny róg domino
+	dir   dir // poziome albo pionowe domino
+	board *Board // referencja do całej tablicy
 }
 
 func (domino *domino) indicies() (int, int) {
@@ -21,24 +21,25 @@ func (domino *domino) indicies() (int, int) {
 }
 
 type Board struct {
-	rows    int
-	columns int
-	board   []bool
+	rows    int // liczba wierszy kratownicy
+	columns int // liczba kolumn kratownicy
+	board   []bool // tablica reprezentująca puste i pełne miejsca na kratownicy
 }
 
 func (board *Board) index(row int, column int) int {
-	return row*board.columns + column
+	return row*board.columns + column // indeks w 1 wymiarowej tablicy emulującej 2 wymiarową
 }
 
 func (board *Board) valid(row int, column int) bool {
-	return row < board.rows && column < board.columns
+	return row < board.rows && column < board.columns // czy pozycja (wiersz,kolumna) nalezy do kratownicy
 }
 
 func (board *Board) Mark(row int, column int) {
-	board.board[board.index(row, column)] = true
+	board.board[board.index(row, column)] = true      // zaznacz pole jako zajęte
 }
 
 func EmptyBoard(rows int, columns int) *Board {
+	// pusta kratonica
 	return &Board{
 		rows,
 		columns,
@@ -47,6 +48,7 @@ func EmptyBoard(rows int, columns int) *Board {
 }
 
 func copyBoard(base Board) *Board {
+	// skopiuj kratownice
 	board := EmptyBoard(base.rows, base.columns)
 	for index := range base.board {
 		board.board[index] = base.board[index]
@@ -55,9 +57,12 @@ func copyBoard(base Board) *Board {
 }
 
 func (b *Board) moves() []*domino {
+	// wszystkie nowe domina ktore mozna dolozyc do kratownicy
 	dominos := []*domino{}
 	for index := range b.board {
+		// dla kazdego pola jesli jest puste (zakladac, ze jest prawy gornym rogiem nowego klocka)
 		if !b.board[index] {
+			// sprawdz czy klocek sie miesci na kratownicy i jest na niego miejsce
 			right := index + 1
 			if b.valid(index/b.columns, index%b.columns+1) && !b.board[right] {
 				dominos = append(dominos, &domino{index, LR, b})
@@ -72,6 +77,7 @@ func (b *Board) moves() []*domino {
 }
 
 func (board *Board) Hash() SolutionHash {
+	// Niezbyt madra funkcja generujaca unikalnego hasha (stringa) dla kazdych 2 roznych kratownic
 	letters := ""
 	for integer := range board.board {
 		if board.board[integer] {
@@ -84,6 +90,7 @@ func (board *Board) Hash() SolutionHash {
 }
 
 func (board *Board) IsSolution() bool {
+	// czy wszystkie pola na kratownicy sa zajete
 	for index := range board.board {
 		if board.board[index] {
 			continue
@@ -95,10 +102,11 @@ func (board *Board) IsSolution() bool {
 }
 
 func (board *Board) Steps() []Step {
-	dominos := board.moves()
+	dominos := board.moves() // dla danej kratownicy , znajdz mozliwe ruchy (nowe domina)
 	boards := make([]Step, 0, len(dominos))
 	for index := range dominos {
-		newboard := copyBoard(*board)
+		// wykonaj mozliwe ruchy, tworzac nowe kratownice
+		newboard := copyBoard(*board) 
 		domino := dominos[index]
 		x, y := domino.indicies()
 		newboard.board[x] = true
--- game/game.go
+++ game/game.go
@@ -2,50 +2,38 @@ package game
 
 import "fmt"
 
-type SolutionHash string
+type SolutionHash string // reprezentacja gwarantujaca unikalnosc kolejnych krokow, zeby sie nie zapetlic
+
 
 type Step interface {
-	Steps() []Step
-	Hash() SolutionHash
-	IsSolution() bool
+	Steps() []Step // kazdy krok generuje kolejne kroki
+	Hash() SolutionHash // kazdy krok posiada unikalna reprezentacje
+	IsSolution() bool // kazdy krok moze byc rozwiazaniem gry
 }
 
-type registry map[SolutionHash]Step
-type status int
-
-const (
-	SUCCESS    status = 0
-	FAILURE    status = 1
-	NEXT_ROUND status = 2
-)
-
-type result struct {
-	status status
-	step   Step
-}
-
+type registry map[SolutionHash]Step // mapa krokow, nie jest synchronizowana, ale z racji ze nie mozna usuwac elementow to wydaje sie to bezpieczne
 func gameRound(frontier chan Step, visited *registry, tokens chan token, results chan<- Step) {
+	// w kolko szukaj rozwiazania
 	for {
-		token := <-tokens
+		token := <-tokens // dziwny mechanizm wykrywania, ze nie ma dalszych krokow do analizy i trzeba oglosic porazke - mozliwe ze niepoprawny
 		if len(tokens) == 0 && len(frontier) == 0 {
 			results <- nil
 		} else {
-			fmt.Printf("Go %d\n", len(tokens))
-			test := <-frontier
-			tokens <- token
-			(*visited)[test.Hash()] = test
+			test := <-frontier // wez krok do analizy
+			tokens <- token // oddaj token
+			(*visited)[test.Hash()] = test // oznacz krok jako juz analizowany
 			if test.IsSolution() {
-				results <- test
+				results <- test // jesli krok jest rozwiazaniem zwroc fo jako sukces
 				break
 			}
+			// jesli aktualny krok nie jest rozwiazaniem to zobacz mozliwe do wykonania kroki
 			steps := test.Steps()
 			for index := range steps {
 				step := steps[index]
 				if (*visited)[step.Hash()] != nil {
-					//ignore
-					fmt.Printf("Ignore\n")
+					// zignoruj juz znane kroki
 				} else {
-					frontier <- step
+					frontier <- step // wrzuc nowy krok do analizy jesli jest nowy
 				}
 			}
 		}
@@ -55,16 +43,16 @@ func gameRound(frontier chan Step, visited *registry, tokens chan token, results
 type token int
 
 func Play(initial Step) Step {
-	visited := &registry{}
-	frontier := make(chan Step)
-	results := make(chan Step)
-	jobsNo := 5
-	tokens := make(chan token, jobsNo)
+	visited := &registry{} // mapa krokow, juz minionych (wystarczyl by zbior hashy, bo teraz zuzywa za duzo pamieci)
+	frontier := make(chan Step) // synchronizowany kanał kroków do analizy
+	results := make(chan Step) // kanał na wyniki (de facto pierwszy)
+	jobsNo := 5 // liczba 'watkow'
+	tokens := make(chan token, jobsNo) // kanal pomocniczy do wykrywania, ze juz nie ma krokow do analizy
 	for index := 0; index < jobsNo; index++ {
 		tokens <- token(index)
-		go gameRound(frontier, visited, tokens, results)
+		go gameRound(frontier, visited, tokens, results) // odpal watki
 	}
-	frontier <- initial
-	result := <-results
+	frontier <- initial // wrzuc stan poczatkowy
+	result := <-results // czekaj na wynik (krok - success, nil - nie ma rozwiazania)
 	return result
 }
